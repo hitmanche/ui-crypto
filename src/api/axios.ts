8dfdef2676ca6ab69e1322541ba8c97dfcbb375f
tayfun akgün <tayfun_akgun@windowslive.com>
2022-02-17 03:37:14 +0300

    Update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,10 +3,7 @@ import { LoginManager } from "src/services/loginManager";
 
 const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://crypto-exchange-meditopia.herokuapp.com/"
-    : "http://localhost:4500/";
+axiosInstance.defaults.baseURL ="https://crypto-exchange-meditopia.herokuapp.com/";
 axiosInstance.defaults.timeout = 25000;
 axiosInstance.defaults.headers.common["Content-Type"] = "application/json";
 axiosInstance.defaults.headers.common["Accept"] = "application/json";
